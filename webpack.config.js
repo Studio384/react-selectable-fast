f801f18f70754ce0dbd1b0fe6a7c31e676258255
Yannick <yannick.franssen@outlook.com>
2019-11-13 14:12:49 +0100

    Drop examples

--- webpack.config.js
+++ webpack.config.js
@@ -28,5 +28,8 @@ module.exports = {
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify(env)
     })
-  ]
+  ],
+  optimization: {
+    providedExports: false
+  }
 }
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -2,43 +2,41 @@ const path = require('path')
 const webpack = require('webpack')
 // const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
 
-const env = process.env.NODE_ENV || 'production'
-
 module.exports = {
   devtool: 'source-map',
-  mode: env,
+  mode: 'production',
   entry: path.resolve(__dirname, 'src', 'index.ts'),
   externals: {
     react: {
       root: 'React',
       commonjs2: 'react',
       commonjs: 'react',
-      amd: 'react',
-    },
+      amd: 'react'
+    }
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
     publicPath: '',
     filename: 'react-selectable-fast.js',
     library: 'React-Selectable-Fast',
-    libraryTarget: 'umd',
+    libraryTarget: 'umd'
   },
   module: {
     rules: [
       {
         test: /\.(js|ts|tsx)$/,
         exclude: /node_modules/,
-        loader: 'babel-loader',
-      },
-    ],
+        loader: 'babel-loader'
+      }
+    ]
   },
   resolve: {
-    extensions: ['.ts', '.tsx', '.js'],
+    extensions: ['.ts', '.tsx', '.js']
   },
   plugins: [
     // new BundleAnalyzerPlugin(),
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(env),
-    }),
-  ],
+      'process.env.NODE_ENV': JSON.stringify('production')
+    })
+  ]
 }
